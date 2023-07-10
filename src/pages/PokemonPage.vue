0cd9bf7b3597caefaddca972a7b933cf8ae8132b
Lissette Romero <romerolissette17@gmail.com>
2023-07-10 11:34:03 -0600

    Visualizar las opciones en el componente

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,19 +1,31 @@
 <template>
   <h1>Adivina el pokemon</h1>
-  <PokemonPicture :pokemonId="12" :showPokemon="true"/>
-  <PokemonOptions />
+  <PokemonPicture :pokemonId="12" :showPokemon="true" />
+  <PokemonOptions :pokemons="pokemonArray" />
 </template>
 
 <script>
-import PokemonOptions from "@/components/PokemonOptions.vue"
+import PokemonOptions from "@/components/PokemonOptions.vue";
 import PokemonPicture from "@/components/PokemonPicture.vue";
 import getPokemonOptions from "@/helpers/getPokemonOptions.js";
 // console.log(getPokemonOptions())
 export default {
-  components: {
-    PokemonPicture,
-    PokemonOptions,
-  },
+  components: { PokemonPicture, PokemonOptions, },
+  data() {
+      return {
+        pokemonArray: [],
+      };
+    },
+    methods: {
+      async mixPokemonArray() {
+        this.pokemonArray = await getPokemonOptions();
+        console.log(this.pokemonArray)
+      },
+    },
+    //ciclo de vida de un component mount
+    mounted() {
+      this.mixPokemonArray();
+    },
 };
 </script>
 
