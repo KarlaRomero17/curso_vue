a9a88921d6cd312d973c46bcf3fcdbadacfdeac0
Lissette Romero <romerolissette17@gmail.com>
2023-07-10 09:01:14 -0600

    04-pokemon-app

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,47 @@
+import PokemonPicture from '@/components/PokemonPicture.vue';
+<template>
+  <div class="pokemon-container">
+    <img
+      :src="pokemonImage"
+      alt="pokemon"
+      class="pokemon-show"
+    />
+    <!-- <img :src="pokemonImage" alt="pokemon" class="fade-in"> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    pokemonImage() {
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.pokemonId}.svg`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.pokemon-show {
+  filter: brightness(0);
+}
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Adivina el pokemon</h1>
+  <PokemonPicture :pokemonId="4" />
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from "@/components/PokemonOptions.vue"
+import PokemonPicture from "@/components/PokemonPicture.vue"
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+};
+</script>
+
+<style>
+</style>
